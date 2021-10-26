995c7c94b23224b3c47802d0d88b6bb0d18d8003
Daniel Dutra <danieldhsd@gmail.com>
2021-10-26 19:38:39 -0300

    Initial commit

---
+++ src/main/java/com/danieldhsd/framework/api/FrameworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.danieldhsd.framework.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danieldhsd/framework/api/FrameworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danieldhsd.framework.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
